4b1f55970585f2b447fa9321af02299bb71eef6e
parisa <parisa@campus-027-034.ucdavis.edu>
2017-01-23 09:58:20 -0800

    an example case to start with

---
+++ gcd.py
@@ -0,0 +1,25 @@
+#  Input: a set of positive integers
+#  Problem: check if the input set numbers have a common prime number factor (GCD > 1)
+
+def gcd(a,b):
+    while b:
+        (a, b) = (b, a % b)
+    return a
+
+def gcd_list(Xs):
+    # return reduce(lambda x,y:gcd(x,y),Xs)
+    result = Xs[0]
+    for x in Xs[1:]:
+        result = gcd(result,x)
+    return result
+
+def has_common_prime(Xs):
+    return gcd_list(Xs) > 1
+
+L1 = [24,54,12]
+L2 = [24,54,12,5]
+
+print L1, gcd_list(L1), has_common_prime(L1)
+print L2, gcd_list(L2), has_common_prime(L2)
+
+
