e791b1d8af39e53ae83b7da55a4581a6b3c57374
parisa <parisa@campus-027-034.ucdavis.edu>
2017-01-24 16:15:07 -0800

    commenting out unneeded parts from alignment2.py

--- case1.py
+++ case1.py
@@ -1,4 +1,4 @@
-import diagnostics
+import alignment2
 
 # example file
 import gcd
@@ -7,6 +7,11 @@ import gcd
 #  Given an inconsistent set of numbers, we are using the diagnostics approach to find the minium inconsistent subset
 
 
+<<<<<<< HEAD
+tax = alignment2.TaxonomyMapping([])
+print tax.allJustifications([24,54,12,5],gcd.gcd)
+=======
 
 print diagnostics.computeAllJust([24,54,12,5],gcd.gcd)
+>>>>>>> ae3e52d931c1680f673c72c30f9cac11511783d7
 
