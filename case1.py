a257e111ebed61c2376c94bd86a8d493323e7f67
parisa <parisa@campus-027-034.ucdavis.edu>
2017-02-01 16:48:16 -0800

    extracting latticeviz

--- case1.py
+++ case1.py
@@ -8,9 +8,11 @@ import gcd
 #  Given an inconsistent set of numbers, we are using the diagnostics approach to find the minium inconsistent subset
 
 tax = diagnostics.lattice([])
-lat = diaglattice.DiagnosticLattice('MIS.txt')
 
 tax.allJustifications([24,54,12,5,9,17],gcd.has_common_prime)
+lat = diaglattice.DiagnosticLattice('MIS.txt')
 
-out = lat.reducedLatViz()
-print out
+
+out = lat.fullLatViz()
+f = open('out.dot', 'w+')
+f.write(out)
